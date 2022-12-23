88c69a3252a45d808a9a1757a560c88c802a565e
john <johnlovesyou@naver.com>
2022-12-23 12:00:29 +0900

    221223(1)

--- client/src/dep/Dep1.js
+++ client/src/dep/Dep1.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import './Dep.css'
 import axios from 'axios'
 import classnames from 'classnames';
+import Basicdepmain from '../depdatabasic/Basicdepmain'
 import Basicgroup from '../depdatabasic/Basicgroup'
 import Date from '../depcommon/Date'
 import Deptable from '../depcommon/Deptable';
@@ -15,21 +16,44 @@ function Dep1(props) {
   let navigate = useNavigate();
 
   useMemo(()=>{ return (
+    axios.get(`/depmain`).then((결과)=>{ //부서별 수정할 것
+      console.log(결과.data)
+      let copy = [...결과.data]
+      set_dmain(copy)
+    }),
     axios.get(`/dep/1`).then((결과)=>{ //부서별 수정할 것
       console.log(결과.data)
       let copy = [...결과.data]
-      setperson(copy)
+      set부서(copy)
     })
   ) }, [])
 
+  let [dmain, set_dmain] = useState(Basicdepmain)
   let [부서, set부서] = useState(Basicgroup)
-  let 학년1_filter = 부서.filter(e => e.an === `1-1`)
-  
-  let [person, setperson] = useState(학년1_filter)
-  let copy = person.map(e => e.dgn_ko)
-  let group = [...new Set(copy)]
 
-  let [show, setshow] = useState('1')
+  // 각학년이름
+  let 학년 = dmain.filter(e => e.dn === `dep1`)
+  let 학년copy = 학년.map(e => e.dan_ko)
+  let 학년result = [...new Set(학년copy)]
+
+  // 1학년
+  let 학년1_person = 부서.filter(e => e.an === `1-1`)
+  let copy1 = 학년1_person.map(e => e.dgn_ko)
+  let group1 = [...new Set(copy1)]
+
+  // 2학년
+  let 학년2_person = 부서.filter(e => e.an === `1-2`)
+  let copy2 = 학년2_person.map(e => e.dgn_ko)
+  let group2 = [...new Set(copy2)]
+
+  // 3학년
+  let 학년3_person = 부서.filter(e => e.an === `1-3`)
+  let copy3 = 학년3_person.map(e => e.dgn_ko)
+  let group3 = [...new Set(copy3)]
+
+  // classNames
+  let [show, setshow] = useState(`${학년result[0]}`)
+  let [color, setcolor] = useState('1')
 
   return (
     <div className='dep_main'>
@@ -38,17 +62,18 @@ function Dep1(props) {
         <div className='dep_main_text'>영유아2부</div>
 
         <div className='dep_main_button1'>
-          <button className='dep_agebutton' onClick={()=>{
-              setshow('1')
-              }}> 1~2세 </button>   
-          <button className='dep_agebutton' onClick={()=>{
-              setshow('2')
-              }}> 3세 </button> 
-          <button className='dep_agebutton' onClick={()=>{
-              setshow('3')
-              }}> 4세 </button> 
-          <button className='dep_agebutton' onClick={()=>{
-              console.log(filter)
+          <button className={classnames('dep_agebutton', {buttoncolor: color ==='1'})} 
+              onClick={()=>{setshow(`${학년result[0]}`); setcolor('1')
+              }}> {학년result[0]} </button>   
+          <button className={classnames('dep_agebutton', {buttoncolor: color ==='2'})} 
+              onClick={()=>{setshow(`${학년result[1]}`); setcolor('2')
+              }}> {학년result[1]} </button> 
+          <button className={classnames('dep_agebutton', {buttoncolor: color ==='3'})} 
+              onClick={()=>{setshow(`${학년result[2]}`); setcolor('3')
+              }}> {학년result[2]} </button> 
+          <button className='dep_agebutton' 
+          onClick={()=>{
+              console.log(학년result)
               }}> 테스트 </button>     
         </div>
 
@@ -56,8 +81,6 @@ function Dep1(props) {
           <button className='dep_dateinputbutton' onClick={()=>{
               navigate('/dateinput')
               }}> 출석 입력하기 </button>   
-          {/* <button className='dep_nameinputtest' onClick={()=>{
-            }}> 테스트 </button>  */}
           <button className='dep_homebutton' onClick={()=>{
               navigate('/')
               }}> 홈버튼 </button> 
@@ -66,17 +89,38 @@ function Dep1(props) {
       </div>
 
       
-      <div className={classnames('dep_main_deptable', {show: show ==='1'})}>
+      <div className={classnames('dep_main_deptable', {show: show === `${학년result[0]}`})}>
         <Date></Date>
         {
-          group.map((a,i)=>{
+          group1.map((a,i)=>{
             return (
-              <Deptable group={group[i]} person={person.filter(e => e.dgn_ko === `${a}`)}></Deptable>
+              <Deptable group={group1[i]} person={학년1_person.filter(e => e.dgn_ko === `${a}`)}></Deptable>
             )
           })  
         }
       </div>
       
+      <div className={classnames('dep_main_deptable', {show: show === `${학년result[1]}`})}>
+        <Date></Date>
+        {
+          group2.map((a,i)=>{
+            return (
+              <Deptable group={group2[i]} person={학년2_person.filter(e => e.dgn_ko === `${a}`)}></Deptable>
+            )
+          })  
+        }
+      </div>
+
+      <div className={classnames('dep_main_deptable', {show: show === `${학년result[2]}`})}>
+        <Date></Date>
+        {
+          group3.map((a,i)=>{
+            return (
+              <Deptable group={group3[i]} person={학년3_person.filter(e => e.dgn_ko === `${a}`)}></Deptable>
+            )
+          })  
+        }
+      </div>
       
       
       
